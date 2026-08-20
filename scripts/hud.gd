eeb8681d232c81a225aa9ae6fcedaabb9c42b897
yarikfanfarik-lgtm <yarik.fanfarik@gmail.com>
2026-08-20 19:15:58 +0300

    Add smoke grenade and mine counters to HUD

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,6 +7,7 @@ var weapon_label: Label
 var score_label: Label
 var crosshair: Label
 var reload_label: Label
+var tactical_label: Label
 
 func setup(p: Node) -> void:
     player = p
@@ -18,10 +19,11 @@ func _build() -> void:
     weapon_label = _label("AK-47", Vector2(1040, 575), 22)
     score_label = _label("KILLS 0", Vector2(28, 575), 22)
     reload_label = _label("", Vector2(510, 620), 24)
+    tactical_label = _label("SMOKE G x2   MINE H x2", Vector2(28, 535), 18)
     crosshair = _label("+", Vector2(632, 348), 30)
     crosshair.horizontal_alignment = HORIZONTAL_ALIGNMENT_CENTER
     crosshair.size = Vector2(20, 30)
-    for label in [hp_label, ammo_label, weapon_label, score_label, reload_label]:
+    for label in [hp_label, ammo_label, weapon_label, score_label, reload_label, tactical_label]:
         label.add_theme_color_override("font_color", Color.WHITE)
         label.add_theme_color_override("font_shadow_color", Color.BLACK)
         label.add_theme_constant_override("shadow_offset_x", 2)
@@ -45,3 +47,9 @@ func _process(_delta: float) -> void:
     reload_label.text = "RELOADING..." if player.reloading else ""
     crosshair.text = "•" if player.aiming else "+"
     crosshair.position = Vector2(632, 348)
+    var tactical_nodes := get_tree().get_nodes_in_group("tactical_manager")
+    if tactical_nodes.is_empty():
+        tactical_label.text = "SMOKE G x2   MINE H x2"
+    else:
+        var tactical = tactical_nodes[0]
+        tactical_label.text = "SMOKE G x%d   MINE H x%d" % [tactical.smoke_count, tactical.mine_count]
